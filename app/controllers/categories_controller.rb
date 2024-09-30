c054632b577869d0516f816c40e5b66be2f6228e
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-09-30 14:36:59 +0100

    add views and front-end

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,30 @@
 class CategoriesController < ApplicationController
-  # before_action :set_list, only: [ :show]
+  before_action :set_category, only: [ :show, :destroy ]
 
   def index
     @categories = Category.all
   end
 
+  def show
+    @bookmark = Bookmark.new
+  end
+
   def new
     @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, status: :see_other
   end
 
   private
