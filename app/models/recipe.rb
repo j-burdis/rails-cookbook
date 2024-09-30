e79d30998a9f205d0d25905f23ec304ff55c7779
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-09-30 10:20:57 +0100

    add associations & validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+  validates :name, :description, presence: true
+  validates :name, uniqueness: true
+  validates :rating, inclusion: { in: 0..10 }
 end
